d0b862b7d6b149bfcebeeaa97746d4509bd7f98b
Andrei Epure <andrei.epure@sonarsource.com>
2020-03-17 18:41:38 +0100

    first commit

---
+++ GetSet/Foo.cs
@@ -0,0 +1,34 @@
+﻿namespace GetSet
+{
+    public class Foo
+    {
+        public int ImplicitProperty { get; set; }
+
+        private string explicitField;
+        public string ExplicitProperty
+        {
+            get
+            {
+                return explicitField;
+            }
+
+            set
+            {
+                explicitField = value;
+            }
+        }
+
+        public bool Method(bool b)
+        {
+            if (b)
+            {
+                ExplicitProperty = "foo";
+            }
+            else
+            {
+                ImplicitProperty = 1;
+            }
+            return b;
+        }
+    }
+}
---
+++ GetSetTests/FooTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using GetSet;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GetSetTests
+{
+    [TestClass]
+    public class FooTests
+    {
+        [TestMethod]
+        public void Method_WithTrue()
+        {
+            Foo f = new Foo();
+            Assert.IsTrue(f.Method(true));
+        }
+
+        [TestMethod]
+        public void Method_WithFalse()
+        {
+            Foo f = new Foo();
+            Assert.IsFalse(f.Method(false));
+        }
+    }
+}
