093ae45e310c0e4b03a8a2acd3df7efa00c10033
Andrei Epure <andrei.epure@sonarsource.com>
2020-03-23 11:51:27 +0100

    leave just setter getter

---
+++ GetSet/Bar.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetSet
+{
+    public class Bar
+    {
+        public int ImplicitProperty { get; set; }
+    }
+}
---
+++ GetSetTests/BarTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using GetSet;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GetSetTests
+{
+    [TestClass]
+    public class BarTests
+    {
+        [TestMethod]
+        public void TestSetter()
+        {
+            Bar bar = new Bar();
+            Assert.AreEqual(0, bar.ImplicitProperty);
+        }
+    }
+}
